69734a4a072d23f8eedbb84b161dfba3e6fc22e0
John <johnlim0521@gmail.com>
2015-05-24 03:11:12 -0700

    got food images

--- Assets/Scripts/FollowingText.cs
+++ Assets/Scripts/FollowingText.cs
@@ -4,20 +4,54 @@ using UnityEngine.UI;
 
 public class FollowingText : MonoBehaviour {
 
-	public GameObject _parent;
 	Text _text;
-
-	[Range(-100f, 100f)]
-	public float xOffset;
-	[Range(-100f, 100f)]
-	public float yOffset;
 
 	// Use this for initialization
 	void Start () {
-		//this.transform.position = _parent.transform.position + Vector3.up;
 		_text = GetComponent<Text> ();
-		_text.text = "HELLO!";
-	
+		_text.color = Color.white;
+		if (this.gameObject.tag == "Applejuice")
+			_text.text = "AppleJuice";
+		else if (this.gameObject.tag == "bread")
+			_text.text = "Bread";
+		else if (this.gameObject.tag == "broccoli")
+			_text.text = "Broccoli";
+		else if (this.gameObject.tag == "cake")
+			_text.text = "Cake";
+		else if (this.gameObject.tag == "cherry")
+			_text.text = "Cherry";
+		else if (this.gameObject.tag == "chicken")
+			_text.text = "Chicken";
+		else if (this.gameObject.tag == "chips")
+			_text.text = "Chips";
+		else if (this.gameObject.tag == "chocolate")
+			_text.text = "Chocolate";
+		else if (this.gameObject.tag == "chowmein")
+			_text.text = "Chowmein";
+		else if (this.gameObject.tag == "cinnaroll")
+			_text.text = "Cinnamon Roll";
+		else if (this.gameObject.tag == "coffee")
+			_text.text = "Coffee";
+		else if (this.gameObject.tag == "egg")
+			_text.text = "Fried Egg";
+		else if (this.gameObject.tag == "fries")
+			_text.text = "French Fries";
+		else if (this.gameObject.tag == "icecream")
+			_text.text = "Icecream";
+		else if (this.gameObject.tag == "kimchi")
+			_text.text = "Kimchi";
+		else if (this.gameObject.tag == "milk")
+			_text.text = "Milk";
+		else if (this.gameObject.tag == "pizza")
+			_text.text = "Pizza";
+		else if (this.gameObject.tag == "potato")
+			_text.text = "Potato";
+		else if (this.gameObject.tag == "salad")
+			_text.text = "Salad";
+		else if (this.gameObject.tag == "sandwich")
+			_text.text = "Sandwich";
+		else if (this.gameObject.tag == "sushi")
+			_text.text = "Sushi";
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/MockSpwn.cs
+++ Assets/Scripts/MockSpwn.cs
@@ -5,7 +5,9 @@ public class MockSpwn: MonoBehaviour {
 
 	[Range(3.0f, 30.0f)]
 	public float radius;
-	public GameObject food;
+	public GameObject apple, bread, broccoli, cake, cherry, chicken,
+	chips, chocolate, chowmein, cinnaroll, coffee, egg, fries, icecream,
+	kimchi, milk, pizza, potato, salad, sandwich, sushi, food;
 
 	float setRadius;
 	Vector3 p;
@@ -26,13 +28,37 @@ public class MockSpwn: MonoBehaviour {
 
 		if (time > 3) {
 
+			int r = Random.Range (0,21);
+
+			if(r == 0) food = apple;
+			else if(r == 1) food = bread;
+			else if(r == 2) food = broccoli;
+			else if(r == 3) food = cake;
+			else if(r == 4) food = cherry;
+			else if(r == 5) food = chicken;
+			else if(r == 6) food = chips;
+			else if(r == 7) food = chocolate;
+			else if(r == 8) food = chowmein;
+			else if(r == 9) food = cinnaroll;
+			else if(r == 10) food = coffee;
+			else if(r == 11) food = egg;
+			else if(r == 12) food = fries;
+			else if(r == 13) food = icecream;
+			else if(r == 14) food = kimchi;
+			else if(r == 15) food = milk;
+			else if(r == 16) food = pizza;
+			else if(r == 17) food = potato;
+			else if(r == 18) food = salad;
+			else if(r == 19) food = sandwich;
+			else if(r == 20) food = sushi;
+
 			float _rand = Random.Range (0f, 360f);
 			Debug.Log ("random number : " + _rand);
 
 			float x = setRadius * Mathf.Cos (_rand) + p.x;
 			float z = setRadius * Mathf.Sin (_rand) + p.z;
 		
-			Instantiate (food, new Vector3 (x, 0, z), Quaternion.Euler(90,0,0));
+			Instantiate (food, new Vector3 (x, 0.2f, z), Quaternion.Euler(90,0,0));
 
 			time = 0;
 		}
