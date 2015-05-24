9dc08e52b2b7843ad8d328c6739bbed3aa319a13
Michael Hiland <mikehilnd@gmail.com>
2015-05-24 12:55:05 -0700

    image reference added for death scene

--- Assets/Cha_Data/Script/Player1Movement.cs
+++ Assets/Cha_Data/Script/Player1Movement.cs
@@ -1,16 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class Player1Movement : MonoBehaviour {
     public static  float movingSpeed = 1;
-  //  public static Image hpBarImage;
+    public static Image hpBarImage;
     AudioSource walkingsound;
     Animation walking;
     // Use this for initialization
     void Start () {
         walkingsound = this.GetComponent<AudioSource>();
         walking = this.GetComponent<Animation>();
-  //      hpBarImage = GameObject.Find("HP Bar_Full").GetComponent<Image>();
+        hpBarImage = GameObject.Find("HP Bar_Full").GetComponent<Image>();
     }
 	
 	
@@ -92,10 +94,10 @@ public class Player1Movement : MonoBehaviour {
         {
             walking.CrossFade("Attack");
         }
-      /*  if (hpBarImage.fillAmount <= 0)
+        if (hpBarImage.fillAmount <= 0)
         {
             //Game Over
             walking.CrossFade("Dead");
-        }*/
+        }
     }
 }
