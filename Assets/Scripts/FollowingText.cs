047950df510232be39512ccfa05b11e010d7061a
John <johnlim0521@gmail.com>
2015-05-23 22:02:07 -0700

    food prefab done

---
+++ Assets/Scripts/FollowingText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FollowingText : MonoBehaviour {
+
+	public GameObject _parent;
+	Text _text;
+
+	[Range(-100f, 100f)]
+	public float xOffset;
+	[Range(-100f, 100f)]
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		//this.transform.position = _parent.transform.position + Vector3.up;
+		_text = GetComponent<Text> ();
+		_text.text = "HELLO!";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//this.transform.position = new Vector3 (_parent.transform.position.x + xOffset, _parent.transform.position.y + yOffset, _parent.transform.position.z);
+
+	
+	}
+}
---
+++ Assets/Scripts/FoodMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FoodMovement : MonoBehaviour {
+
+	public GameObject player1;
+	[Range(0.1f, 3f)]
+	public float speed;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		this.transform.position = new Vector3 (player1.transform.position.x + 500, player1.transform.position.y + 500, player1.transform.position.z);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs ((this.transform.position - player1.transform.position).magnitude) > 5f) {
+			this.transform.position = Vector3.MoveTowards (this.transform.position, player1.transform.position, speed);
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "player1") {
+			Debug.Log ("contacted");
+			Destroy(this.gameObject);
+		}
+
+	}
+}
