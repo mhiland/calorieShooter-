f0dc942e6a29daf86a50459981fd3553b310ffe2
Michael Hiland <mikehilnd@gmail.com>
2015-05-23 20:29:46 -0700

    player 1 movement

---
+++ CalorieShooter/Assets/Cha_Data/Script/Player1Movement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+			this.transform.rotation = Quaternion.Euler(180, 90, 90);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+			this.transform.rotation = Quaternion.Euler(0, 90, 90);
+		//	this.transform.Rotate(0, 0, 90);
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z++; 
+			this.transform.position = position;
+			this.transform.rotation = Quaternion.Euler(90, 90, 90);
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z--;
+			this.transform.position = position;
+			this.transform.rotation = Quaternion.Euler(270, 90, 90);
+		}
+	}
+}
