feb4dbf421a1815d8b1230acde6d171953fdc0ef
John <johnlim0521@gmail.com>
2015-05-23 23:50:12 -0700

    no message

---
+++ Assets/Scripts/MockSpwn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MockSpwn: MonoBehaviour {
+
+	[Range(3.0, 30.0)]
+	public float radius;
+
+	float setRadius;
+
+	// Use this for initialization
+	void Start () {
+		setRadius = Mathf.PI * radius ^ 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
